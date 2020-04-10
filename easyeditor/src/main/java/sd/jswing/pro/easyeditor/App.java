d5ce66cff772c02e71ab148148e13b50d52280af
HaJiBeBeautiful <13977373445@163.com>
2020-04-10 23:20:10 +0800

    新建项目--简单的编辑器

---
+++ easyeditor/src/main/java/sd/jswing/pro/easyeditor/App.java
@@ -0,0 +1,13 @@
+package sd.jswing.pro.easyeditor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
